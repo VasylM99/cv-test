9dd8bd3f201fe9fe6a2e60a1b8ff772d7b8df61b
Misha Kalutskii <78737127+Misha-ship@users.noreply.github.com>
2022-03-18 14:33:48 +0200

    mY COMIT

---
+++ workers-form.php
@@ -0,0 +1,33 @@
+<?php
+print_r($_GET);
+/*
+$name = $_GET['name'];
+$surname = $_GET['surname'];
+$country = $_GET['country'];
+$age = $_GET['age'];
+$phone = $_GET['phone'];
+$email = $_GET['email'];
+$social_pl = $_GET['sc-plat'];
+$social_link = $_GET['sc-link'];
+$has_dl = $_GET['has-dl'];
+$driver_cat = $_GET['driver-cat'];
+$doc_visa = $_GET['doc-visa'];
+$city = $_GET['city'];
+$salary = $_GET['salary'];
+$negotiated_salary = $_GET['negotiated-salary'];
+$language = $_GET['language'];
+$language_skills = $_GET['language-skills'];
+$photo = $_GET['photo'];
+$job_cat = $_GET['job-cat'];
+$career_objective = $_GET['career-objective'];
+$type_employment = $_GET['type_employment'];
+$dop_edc = $_GET['dop-edc'];
+$description = $_GET['description'];
+
+global $wpdb;
+*/
+
+
+
+
+
